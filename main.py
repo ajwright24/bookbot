5dba4cd71d1458cf071e024ba42b2a57f307d750
ajwright24 <ajwright2495@yahoo.com>
2026-01-02 03:37:51 +0800

    Finished project

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+import sys
+
 def get_book_text(file):
     with open(file) as f: # f is a file object
         return f.read()
     
 from stats import count_words
 from stats import count_unique_char
+from stats import sorted_list
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     contents = get_book_text(book_path)
     total_words = count_words(contents)
     unique_char_dict = count_unique_char(contents)
+    sorted_output = sorted_list(unique_char_dict)
+    
+    print("========BOOKBOT========")
+    print(f"Analyzing book found at {book_path}")
+    print ("--------Word Count--------")
     print(f"Found {total_words} total words")
-    print(unique_char_dict)
+    print("--------Character Count--------")
+    for d in sorted_output:
+        if d["name"].isalpha():
+            print(f"{d["name"]}: {d["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -17,12 +17,28 @@ def count_unique_char(contents):
 
 
 
-#loop each key and make it the value of the new key "char"
-# make a key value pair with the new key "num" and the value the count
-# call a function that takes a dictionary and sorts it by number greatest to least (not created yet)
+# create an empty list to store the dictionaries 
+# create a new dictionary with two k:v inputs, {name: "", num: ""}
+# loop the data from the unique characters into the dictionary
+# pass the information into the dictionaries then append them to the list of dictionaries
+# call a helper function that takes a list of dictionaries and sorts it by number greatest to least
 # return the sorted dictionary 
 
-#def sorted_list(dict):
-    #for key in dict:
+def sorted_list(dict):
+    list_of_dictionaries = []
+
+    for k, v in dict.items():
+        char_dict = {"name": k, "num": v}
+        list_of_dictionaries.append(char_dict)
+    list_of_dictionaries.sort(key=sort_on, reverse=True)
+    return list_of_dictionaries
+
+    #for d in list_of_dictionaries:
+    #    print(f"{d["name"]}: {d["num"]}")
+
+def sort_on(item):
+    return item["num"]
+
+    
 
 
